f64a76d81b749c7931b35a9aefdc3e56561ca456
Valerie <valeriejeanchen@gmail.com>
2014-05-01 23:14:34 -0400

    object positions on path

---
+++ Cuckoo/cuckoo.js
@@ -0,0 +1,30 @@
+// var circx = 150;
+// var circy = 200;
+// // var counter = 0;
+
+
+
+// function setup(){
+// 	createCanvas(1400, 800);
+// }
+
+// function draw(){
+// 	background(100, 120, 20);
+// 	// counter++;
+// 	var velx = random(0, 1);
+// 	var vely = random(-1, 1);
+// 	fill(200, 200, 0);
+// 	noStroke();
+// 	ellipse(circx, circy, 20, 20);
+
+// 	circx += velx;
+// 	circy += vely;
+
+// 	if (circx >= width){
+// 		velx = -velx;
+// 	}
+
+// 	if (circy >= height || circy < height) {
+// 		vely = -vely;
+// 	}
+// }
